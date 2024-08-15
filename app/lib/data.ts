cae0582775a69c30e3729cf37f0cc697871fe68f
Ollie <ollie.nicholson@gmail.com>
2024-08-15 12:50:09 +1000

    added search for invoices

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -70,6 +70,13 @@ export async function fetchCardData() {
     const numberOfCustomers = Number(data[1].rows[0].count ?? '0');
     const totalPaidInvoices = formatCurrency(data[2].rows[0].paid ?? '0');
     const totalPendingInvoices = formatCurrency(data[2].rows[0].pending ?? '0');
+    
+    console.log({
+      numberOfInvoices,
+      numberOfCustomers,
+      totalPaidInvoices,
+      totalPendingInvoices,
+    })
 
     return {
       numberOfCustomers,
--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,12 @@
 /** @type {import('next').NextConfig} */
 
-const nextConfig = {};
+const nextConfig = {
+  // Using Partial Pre-rendering requires the latest@next-canary version.
+  // You may not notice the difference in development, although PPR will improve performance in production.
+  // Only use this if you want to use next-canary - which may cause conflicts in your project.
+  //   experimental: {
+  //     ppr: "incremental",
+  //   },
+};
 
 export default nextConfig;
